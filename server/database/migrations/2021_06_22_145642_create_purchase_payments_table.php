aa1d4313cd6b3470bc79eae5b50b3fe4bf9f3d50
tersawy <mohamedtersawy96@gmail.com>
2021-07-10 21:34:35 +0200

    added purchase payment migration

---
+++ server/database/migrations/2021_06_22_145642_create_purchase_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\PurchasePayment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('amount');
+            $table->bigInteger('payment_method')->default(PurchasePayment::PAYMENT_CASH);
+            $table->text('note')->nullable();
+
+            $table->unsignedBigInteger('purchase_id');
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_payments');
+    }
+}
