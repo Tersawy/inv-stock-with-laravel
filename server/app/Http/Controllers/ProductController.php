e70339970e3138c47acd1c0dcf0d45777a8a11b8
tersawy <mohamedtersawy96@gmail.com>
2021-07-10 21:19:03 +0200

    fix productOptions by remove break operator from for each and replace it with else after if, added images to options, added details() methods to get details for each product by id

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use App\Models\ProductVariant;
 use App\Requests\ProductRequest;
+use GuzzleHttp\Handler\Proxy;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
@@ -15,19 +16,19 @@ class ProductController extends Controller
 {
   public function index()
   {
-    $category = ["category" => function ($query) {
+    $category = ['category' => function ($query) {
       $query->select(['id', 'name']);
     }];
 
-    $brand = ["brand" => function ($query) {
+    $brand = ['brand' => function ($query) {
       $query->select(['id', 'name']);
     }];
 
-    $unit = ["unit" => function ($query) {
+    $unit = ['unit' => function ($query) {
       $query->select(['id', 'short_name']);
     }];
 
-    $images = ["images" => function ($query) {
+    $images = ['images' => function ($query) {
       $query->select(['name', 'product_id']);
     }];
 
@@ -55,11 +56,17 @@ class ProductController extends Controller
 
   public function options()
   {
-    $variants = ["variants" => function ($query) {
+    $variants = ['variants' => function ($query) {
       $query->select(['id', 'name', 'product_id']);
     }];
 
-    $products = Product::with($variants)->get(['id', 'name', 'code', 'has_variants']);
+    $images = ['images' => function ($query) {
+      $query->select(['name', 'product_id']);
+    }];
+
+    $withFields = array_merge([], $variants, $images);
+
+    $products = Product::with($withFields)->get(['id', 'name', 'code', 'has_variants']);
 
     $newProducts = [];
 
@@ -67,22 +74,47 @@ class ProductController extends Controller
       $newProduct = [
         'id'    => $product->id,
         'name'  => $product->name,
-        'code'  => $product->code
+        'code'  => $product->code,
+        'image' => count($product->images) ? $product->images[0]['name'] : 'product_empty.jpeg',
       ];
 
       if (!$product->has_variants) {
         $newProducts[] = $newProduct;
-        break;
-      }
-
-      foreach ($product->variants as $variant) {
-        $newProduct['variant'] = $variant->name;
-        $newProduct['variant_id'] = $variant->id;
-        $newProducts[] = $newProduct;
+      } else {
+        foreach ($product->variants as $variant) {
+          $newProduct['variant'] = $variant->name;
+          $newProduct['variant_id'] = $variant->id;
+          $newProducts[] = $newProduct;
+        }
       }
     }
 
+    // return $this->success($products);
     return $this->success($newProducts);
+  }
+
+
+  public function details(Request $req)
+  {
+    $req->merge(['id' => $req->route('id')]);
+
+    $req->validate(['id' => ['required', 'numeric', 'min:1']]);
+
+    $saleUnit = ['sale_unit' => function ($query) {
+      $query->select(['id', 'short_name', 'value', 'operator']);
+    }];
+
+    $purchaseUnit = ['purchase_unit' => function ($query) {
+      $query->select(['id', 'short_name', 'value', 'operator']);
+    }];
+
+    $withFields = array_merge([], $saleUnit, $purchaseUnit);
+
+    $columns = ['id', 'purchase_unit_id', 'sale_unit_id', 'cost', 'price', 'tax', 'tax_method', 'instock'];
+
+    $product = Product::with($withFields)->find($req->id, $columns);
+
+    return $this->success($product->details());
   }
 
 
@@ -92,11 +124,11 @@ class ProductController extends Controller
 
     $req->validate(['id' => ['required', 'numeric', 'min:1']]);
 
-    $images = ["images" => function ($query) {
+    $images = ['images' => function ($query) {
       $query->select(['name', 'product_id']);
     }];
 
-    $variants = ["variants" => function ($query) {
+    $variants = ['variants' => function ($query) {
       $query->select(['name', 'product_id']);
     }];
 
@@ -122,7 +154,7 @@ class ProductController extends Controller
 
     if ($req->has_variants) $this->createVariants($req, $product);
 
-    return $this->success([], "The Product has been created successfully");
+    return $this->success([], 'The Product has been created successfully');
   }
 
 
@@ -138,7 +170,7 @@ class ProductController extends Controller
 
     $product = Product::find($req->id);
 
-    if (!$product) return $this->error("The product was not found", 404);
+    if (!$product) return $this->error('The product was not found', 404);
 
     $product->fill($attr);
 
@@ -174,7 +206,7 @@ class ProductController extends Controller
 
     if ($req->has_variants) $this->createVariants($req, $product);
 
-    return $this->success(Arr::pluck($oldVariants, 'id'), "The Product has been updated successfully");
+    return $this->success(Arr::pluck($oldVariants, 'id'), 'The Product has been updated successfully');
   }
 
 
@@ -184,7 +216,7 @@ class ProductController extends Controller
 
     foreach ($files as $file) {
 
-      $imageInfo = explode(";base64,", $file);
+      $imageInfo = explode(';base64,', $file);
 
       $ext = '.' . str_replace('data:image/', '', $imageInfo[0]);
 
