977c85c17bc9929327195de14cc58384161c6133
tersawy <mohamedtersawy96@gmail.com>
2021-07-12 03:05:44 +0200

    added purchase payments routes

--- server/routes/api/purchase.php
+++ server/routes/api/purchase.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\PurchasePaymentController;
 
 Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'purchase'], function () {
-    Route::get('/', [PurchaseController::class, 'index']);
 
-    Route::get('/options', [PurchaseController::class, 'options']);
+    Route::get('/', [PurchaseController::class, 'index']);
 
     Route::get('/{id}', [PurchaseController::class, 'show']);
 
-    Route::put('/{id}', [PurchaseController::class, 'update']);
-
     Route::post('/create', [PurchaseController::class, 'create']);
+
+    Route::put('/{id}', [PurchaseController::class, 'update']);
 
     Route::post('/{id}/trash', [PurchaseController::class, 'moveToTrash']);
 
@@ -21,4 +21,19 @@ Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'purchase'], functio
     Route::post('/{id}/restore', [PurchaseController::class, 'restore']);
 
     Route::post('/{id}', [PurchaseController::class, 'remove']);
+});
+
+############################################# Purchase Payment #############################################
+
+Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'purchase/{purchaseId}/payments'], function () {
+
+    Route::get('/', [PurchasePaymentController::class, 'index']);
+
+    Route::get('/{id}', [PurchasePaymentController::class, 'show']);
+
+    Route::post('/create', [PurchasePaymentController::class, 'create']);
+
+    Route::put('/{id}', [PurchasePaymentController::class, 'update']);
+
+    Route::post('/{id}', [PurchasePaymentController::class, 'remove']);
 });
