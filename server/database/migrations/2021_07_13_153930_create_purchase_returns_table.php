8021c60843ca2adf80c44851eabe11e5990ce795
tersawy <mohamedtersawy96@gmail.com>
2021-07-14 23:35:13 +0200

    added purchase return migration table

---
+++ server/database/migrations/2021_07_13_153930_create_purchase_returns_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Models\Product;
+use App\Models\PurchaseReturn;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseReturnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_returns', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('tax')->default(0);
+            $table->integer('discount')->default(0);
+            $table->tinyInteger("discount_method")->default(Product::DISCOUNT_FIXED); // 0 Fixed, 1 Percent
+            $table->tinyInteger('status')->default(PurchaseReturn::COMPLETED);
+            $table->integer('shipping')->default(0);
+
+            $table->text('note')->nullable();
+            $table->date('date')->default(date('Y-m-d'));
+
+            $table->unsignedBigInteger('supplier_id');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('restrict');
+
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_Returns');
+    }
+}
