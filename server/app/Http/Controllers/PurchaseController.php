a60d2f6c8bd3d91df866e0373c74bcafa5d97250
tersawy <mohamedtersawy96@gmail.com>
2021-07-17 11:16:33 +0200

    some refactoring for update() method, and maybe this method need to check quantity after subtract as like update() method in PurchaseReturnController

--- server/app/Http/Controllers/PurchaseController.php
+++ server/app/Http/Controllers/PurchaseController.php
@@ -112,46 +112,29 @@ class PurchaseController extends Controller
         # 1 - Make validation for request update rule
         # 2 - Make Distinct validation for new details
         # 3 - Get Purchase with details
-        # 4 - Split new and old details in two separate variables
+        # 4 - Get old details
         # 5 - Set purchase id into every new detail
-        # 6 - Get products IDs from details for every type of detail separatly => oldProductsIds, newProductsId
-        # 7 - Merge all ids to get all Products
-        # 8 - Get all Products
-        # 9 - Merge all details to check variants with products
-        # 10 - Check Variants with Products => eg. if product has a variant and detail doesn't has a variant,
+        # 6 - Get all Products by ids
+        # 7 - Merge all details to check variants with products
+        # 8 - Check Variants with Products => eg. if product has a variant and detail doesn't has a variant,
             the purchase was created a long time ago with products doesn't have variants then
             we updated product by added variant to it then we want to update this purchase with this product out variant !
             whats happen without this check ?!!
             it will be add quantity into product.instock but product has variant supposed to added quantity into variant.instock
-        # 11 - check if new or old status is received to get variants and details to check relations between products with them variants
-        # 12 - Subtract quantity from old products has not variants and variants by old details
-            A) check if old status is received
-                1- filter old details has variants and old details not has variants from old details
-                2- if count of old details that has variants > 0
-                    I) subtract quantity from old variants
-                3- if count of old details that hasn't variants > 0
-                    I) filter products has no variants
-                   II) subtract quantity from products has no variants
-        # 13 - Sum quantity to new products has not variants and variants by new details
-            A) check if new status is received
-                1- filter new details has variants and new details not has variants from new details
-                2- if count of new details that has variants > 0
-                    I) sum quantity to new variants
-                3- if count of new details that hasn't variants > 0
-                    I) sum quantity to new product
-        # 14 - delete old details from `purchase_details` by $purchase->id
-        # 15 - filter products has variants
-        # 16 - update multiple products has variants
-        # 17 - update multiple variants
-        # 18 - update purchase with new data
-        # 19 - insert new details
+        # 9  - Filter details to get whose has variant
+        # 10 - check if new or old status is received and count of detailsHasVariants > 1 to get variants and details to check relations between products with them variants
+        # 11 - Subtract quantity from old products has not variants and variants by old details
+        # 12 - Sum quantity to new products has not variants and variants by new details
+        # 13 - delete old details from `purchase_details` by $purchase->id
+        # 14 - if old or new status is received update multiple products and variants
+        # 15 - update purchase with new data
+        # 16 - insert new details
      */
     public function update(Request $req, $id)
     {
         # [1]
         $attr = PurchaseRequest::validationUpdate($req);
 
-        # [4]
         $newDetails = &$attr['products'];
 
         # [2]
@@ -173,112 +156,71 @@ class PurchaseController extends Controller
             $detail['variant_id'] = Arr::get($detail, 'variant_id');
         }
 
-        # [6]
         $oldProductsIds = Arr::pluck($oldDetails, 'product_id');
 
-        # [6]
         $newProductsIds = Arr::pluck($newDetails, 'product_id');
 
-        # [7]
         $ids = [...$oldProductsIds, ...$newProductsIds];
 
-        # [8]
+        # [6]
         $products = Product::find($ids);
 
-        # [9]
+        # [7]
         $allDetails = [...$newDetails, ...$oldDetails];
 
-        # [10]
+        # [8]
         list($isValid, $errMsg) = $this->checkProductsWithVariants($allDetails, $products);
 
         if (!$isValid) return $this->error($errMsg, 422);
 
-        # [11]
-        if ($purchase->status == Purchase::RECEIVED || $req->status == Purchase::RECEIVED) {
+        $oldIsReceived = $purchase->status == Purchase::RECEIVED;
 
-            $allDetailsHasVariants = $this->filterDetailsVariants($allDetails, true);
+        $newIsReceived = $req->status == Purchase::RECEIVED;
 
-            if (count($allDetailsHasVariants)) {
+        # [9]
+        $detailsHasVariants = $this->filterDetailsVariants($allDetails, true);
 
-                $variants = $this->getVariants($allDetailsHasVariants);
+        $variants = [];
 
-                list($isValid, $errMsg) = $this->checkingRelations($allDetailsHasVariants, $variants, $products);
+        # [10]
+        if (($newIsReceived || $oldIsReceived) && count($detailsHasVariants)) {
 
-                if (!$isValid) return $this->error($errMsg, 422);
-            }
+            $variants = $this->getVariants($detailsHasVariants);
+
+            list($isValid, $errMsg) = $this->checkingRelations($detailsHasVariants, $variants, $products);
+
+            if (!$isValid) return $this->error($errMsg, 422);
         }
 
-        # [12] Subtract Old Quantity
-        # [12] => A)
-        if ($purchase->status == Purchase::RECEIVED) {
-
-            # [12] => A) => 1
-            $oldDetailsHasVariants = $this->filterDetailsVariants($oldDetails, true);
-            # [12] => A) => 1
-            $oldDetailsHasNoVariants = $this->filterDetailsVariants($oldDetails, false);
-
-            # [12] => A) => 2
-            if (count($oldDetailsHasVariants)) {
-                # [12] => A) => 2 => I)
-                $this->subtractVariantsQuantity($variants, $oldDetailsHasVariants);
-            }
-
-            # [12] => A) => 3
-            if (count($oldDetailsHasNoVariants)) {
-                # [12] => A) => 3 => I)
-                $productsHasNoVariants = $this->filterProductsVariants($products, false);
-                # [12] => A) => 3 => II)
-                $this->subtractProductsQuantity($productsHasNoVariants, $oldDetailsHasNoVariants);
-            }
+        # [11] Subtract Old Quantity
+        if ($oldIsReceived) {
+            $this->subtractQuantity($variants, $oldDetails, $products);
         }
 
-        # [13] Sum New Quantity
-        # [13] => A)
-        if ($req->status == Purchase::RECEIVED) {
-
-            # [13] => A) => 1
-            $newDetailsHasVariants = $this->filterDetailsVariants($newDetails, true);
-            # [13] => A) => 1
-            $newDetailsHasNoVariants = $this->filterDetailsVariants($newDetails, false);
-
-            # [13] => A) => 2
-            if (count($newDetailsHasVariants)) {
-                # [13] => A) => 2 => I)
-                $this->sumVariantsQuantity($variants, $newDetailsHasVariants);
-            }
-
-            # [13] => A) => 3
-            if (count($newDetailsHasNoVariants)) {
-                # [13] => A) => 3 => I)
-                $productsHasNoVariants = $this->filterProductsVariants($products, false);
-                # [13] => A) => 3 => II)
-                $this->sumProductsQuantity($productsHasNoVariants, $newDetailsHasNoVariants);
-            }
+        # [12] Sum New Quantity
+        if ($newIsReceived) {
+            $this->sumQuantity($variants, $newDetails, $products);
         }
 
-        # [14]
+        # [13]
         PurchaseDetail::where('purchase_id', $purchase->id)->delete();
 
-        # [15]
-        $productsHasNoVariants = $this->filterProductsVariants($products, false);
+        # [14]
+        if ($oldIsReceived || $newIsReceived) {
 
-        # [16]
-        if (count($productsHasNoVariants)) {
+            $productsHasNoVariants = $this->filterProductsVariants($products, false);
+
             $this->updateMultiple($productsHasNoVariants, Product::class, 'instock');
-        }
 
-        # [17]
-        if (count($variants)) {
             $this->updateMultiple($variants, ProductVariant::class, 'instock');
         }
 
-        # [18]
         $purchase->fill($attr);
 
-        # [18]
+        # [15]
         $purchase->save();
 
-        # [19]
+        # [16]
         PurchaseDetail::insert($newDetails);
 
         return $this->success([], "The Purchase has been updated successfully");
