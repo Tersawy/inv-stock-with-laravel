a6439d1909879cc2426698ee9972b87c3c1c33ed
tersawy <mohamedtersawy96@gmail.com>
2021-07-17 23:51:06 +0200

    Added migrate sale_details table

---
+++ server/database/migrations/2021_07_17_194838_create_sale_details_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Helpers\Constants;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaleDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer("price");
+            $table->integer("quantity");
+            $table->integer('tax')->default(0);
+            $table->tinyInteger("tax_method")->default(Constants::TAX_EXCLUSIVE); // 0 Exclusive, 1 Inclusive
+            $table->integer('discount')->default(0);
+            $table->tinyInteger("discount_method")->default(Constants::DISCOUNT_FIXED); // 0 Fixed, 1 Percent
+
+            $table->bigInteger('variant_id')->nullable();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedBigInteger('sale_id');
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_details');
+    }
+}
