204020b5925a6f224d82b2f25c904be7315bf935
tersawy <mohamedtersawy96@gmail.com>
2021-07-17 23:51:40 +0200

    Added SaleDetail Model

---
+++ server/app/Models/SaleDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\ProductDetailsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleDetail extends Model
+{
+    use ProductDetailsAttributes;
+
+    protected $fillable = [
+        'product_id',
+        'sale_id',
+        'price',
+        'tax',
+        'tax_method',
+        'discount',
+        'discount_method',
+        'quantity'
+    ];
+}
