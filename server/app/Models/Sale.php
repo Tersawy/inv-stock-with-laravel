6deafaf7b4fbb290292ea4a070c708cd9714d5d8
tersawy <mohamedtersawy96@gmail.com>
2021-07-17 23:39:54 +0200

    Added Sale Model

---
+++ server/app/Models/Sale.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\InvoiceAttributes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Sale extends Model
+{
+    use SoftDeletes, InvoiceAttributes;
+
+    protected $fillable = [
+        'warehouse_id',
+        'customer_id',
+        'tax',
+        'discount',
+        'discount_method',
+        'status',
+        'shipping',
+        'note',
+        'date'
+    ];
+
+    protected $append = ['reference'];
+
+    protected $fieldActionName = 'price';
+
+    public function details()
+    {
+        return $this->hasMany(SaleDetail::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(SalePayment::class);
+    }
+
+    public function getReferenceAttribute()
+    {
+        return 'SL_' . (1110 + $this->id);
+    }
+}
