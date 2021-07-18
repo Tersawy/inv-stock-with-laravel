f0b90430bae423dfdaac022cb94b9c38cfd2f69a
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 08:07:29 +0200

    Added Sale Return routes with payments routes

---
+++ server/routes/api/sale_return.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleReturnController;
+use App\Http\Controllers\SaleReturnPaymentController;
+
+Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'sale-return'], function () {
+
+    Route::get('/', [SaleReturnController::class, 'index']);
+
+    Route::get('/trashed', [SaleReturnController::class, 'trashed']);
+
+    Route::get('/{id}', [SaleReturnController::class, 'show']);
+
+    Route::post('/create', [SaleReturnController::class, 'create']);
+
+    Route::put('/{id}', [SaleReturnController::class, 'update']);
+
+    Route::post('/{id}/trash', [SaleReturnController::class, 'moveToTrash']);
+
+    Route::post('/{id}/restore', [SaleReturnController::class, 'restore']);
+
+    Route::post('/{id}', [SaleReturnController::class, 'remove']);
+});
+
+############################################# Sale Return Payment #############################################
+
+Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'sale-return/{saleId}/payments'], function () {
+
+    Route::get('/', [SaleReturnPaymentController::class, 'index']);
+
+    Route::get('/{id}', [SaleReturnPaymentController::class, 'show']);
+
+    Route::post('/create', [SaleReturnPaymentController::class, 'create']);
+
+    Route::put('/{id}', [SaleReturnPaymentController::class, 'update']);
+
+    Route::post('/{id}', [SaleReturnPaymentController::class, 'remove']);
+});
