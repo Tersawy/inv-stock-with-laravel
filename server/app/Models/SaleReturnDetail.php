ab0034594180a61ef253f3d7da30f010c328cf5a
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 08:03:22 +0200

    Added SaleReturnDetail Model

---
+++ server/app/Models/SaleReturnDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\ProductDetailsAttributes;
+
+class SaleReturnDetail extends Model
+{
+    use ProductDetailsAttributes;
+
+    protected $fillable = [
+        'product_id',
+        'sale_return_id',
+        'price',
+        'tax',
+        'tax_method',
+        'discount',
+        'discount_method',
+        'quantity'
+    ];
+}
