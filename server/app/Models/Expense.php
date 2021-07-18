a08c3ce62c269552e182064ce58fffec43144cc2
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 09:15:15 +0200

    Added Expense Model

---
+++ server/app/Models/Expense.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ExpenseCategory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Expense extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['amount', 'expense_category_id', 'warehouse_id', 'date', 'details'];
+
+    public function category()
+    {
+        return $this->belongsTo(ExpenseCategory::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+}
