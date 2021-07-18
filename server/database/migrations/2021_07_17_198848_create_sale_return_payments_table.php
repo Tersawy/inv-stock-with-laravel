cb436ea48a3a8dcb31dcd243f787b466527b7d80
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 08:02:35 +0200

    Added migration Sale Return Payments table

---
+++ server/database/migrations/2021_07_17_198848_create_sale_return_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Helpers\Constants;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaleReturnPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_return_payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('amount');
+            $table->bigInteger('payment_method')->default(Constants::PAYMENT_CASH);
+            $table->text('note')->nullable();
+
+            $table->unsignedBigInteger('sale_return_id');
+            $table->foreign('sale_return_id')->references('id')->on('sale_returns')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_return_payments');
+    }
+}
