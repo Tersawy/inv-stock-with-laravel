de14ea9b55e5d323cff5cb97e04b0217c4d640d3
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 07:50:39 +0200

    Added migration Sale Return table

---
+++ server/database/migrations/2021_07_17_196930_create_sale_returns_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Helpers\Constants;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaleReturnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_returns', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('tax')->default(0);
+            $table->integer('discount')->default(0);
+            $table->tinyInteger("discount_method")->default(Constants::DISCOUNT_FIXED); // 0 Fixed, 1 Percent
+            $table->tinyInteger('status')->default(Constants::INVOICE_RETURN_COMPLETED);
+            $table->integer('shipping')->default(0);
+
+            $table->text('note')->nullable();
+            $table->date('date')->default(date('Y-m-d'));
+
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('restrict');
+
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_Returns');
+    }
+}
