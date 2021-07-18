2f8ae0a41089f9cbfd42ba0190ec6d87f51f980a
tersawy <mohamedtersawy96@gmail.com>
2021-07-18 09:04:18 +0200

    Added migration Expense table

---
+++ server/database/migrations/2021_07_18_065548_create_expenses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('amount');
+            $table->string('details');
+            $table->date('date')->default(date('Y-m-d'));
+
+            $table->unsignedBigInteger('expense_category_id')->nullable();
+            $table->foreign('expense_category_id')->references('id')->on('expenses')->onDelete('restrict');
+
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
