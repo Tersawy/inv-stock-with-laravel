59aafd294467d2646f748f4f6c41b20af5edf894
tersawy <mohamedtersawy96@gmail.com>
2021-07-19 19:54:21 +0200

    Set operator field for mainUnit '*'

--- server/database/seeds/SubUnitSeeder.php
+++ server/database/seeds/SubUnitSeeder.php
@@ -13,9 +13,9 @@ class SubUnitSeeder extends Seeder
     public function run()
     {
         DB::table('sub_units')->insert([
-            ['name' => 'Dezon Box', 'short_name' => 'Box',  'value' => 1,       'main_unit_id' => 1],
+            ['name' => 'Dezon Box', 'short_name' => 'Box',  'value' => 1,       'main_unit_id' => 1, 'operator' => '*'],
             ['name' => 'piece',     'short_name' => 'pc',   'value' => 12,      'main_unit_id' => 1],
-            ['name' => 'ton',       'short_name' => 'ton',  'value' => 1,       'main_unit_id' => 2],
+            ['name' => 'ton',       'short_name' => 'ton',  'value' => 1,       'main_unit_id' => 2, 'operator' => '*'],
             ['name' => 'Kilogram',  'short_name' => 'Kg',   'value' => 1000,    'main_unit_id' => 2],
             ['name' => 'Gram',      'short_name' => 'g',    'value' => 1000000, 'main_unit_id' => 2],
         ]);
