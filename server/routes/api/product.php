49e8da332cb3a5362739437bb0f16bd509e8494b
tersawy <mohamedtersawy96@gmail.com>
2021-07-21 18:59:09 +0200

    Added Product Variant Routes

--- server/routes/api/product.php
+++ server/routes/api/product.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductVariantController;
 
 Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'product'], function () {
     Route::get('/', [ProductController::class, 'index']);
@@ -23,4 +24,19 @@ Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'product'], function
     Route::post('/{id}/restore', [ProductController::class, 'restore']);
 
     Route::post('/{id}', [ProductController::class, 'remove']);
+});
+
+############################################ Product Variant ############################################
+
+Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'product/{productId}/variant'], function () {
+
+    Route::get('/', [ProductVariantController::class, 'index']);
+
+    Route::get('/{id}', [ProductVariantController::class, 'show']);
+
+    Route::put('/{id}', [ProductVariantController::class, 'update']);
+
+    Route::post('/create', [ProductVariantController::class, 'create']);
+
+    Route::post('/{id}', [ProductVariantController::class, 'remove']);
 });
