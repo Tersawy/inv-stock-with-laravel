98e18a8647e683f2b22f3a70a345272dc81e10d2
tersawy <mohamedtersawy96@gmail.com>
2021-07-21 19:01:17 +0200

    Added ProductVariantImage Model

---
+++ server/app/Models/ProductVariantImage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariantImage extends Model
+{
+    protected $fillable = ['name', 'product_variant_id'];
+
+    protected $appends = ['path'];
+
+    public function getPathAttribute()
+    {
+        $type = pathinfo($this->name, PATHINFO_EXTENSION);
+
+        $imagePath = public_path('/images/products/') . $this->name;
+
+        if (file_exists($imagePath)) {
+            $data = file_get_contents($imagePath);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+            return $base64;
+        }
+    }
+}
