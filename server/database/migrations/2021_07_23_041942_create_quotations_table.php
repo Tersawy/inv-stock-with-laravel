69601dcae1835177e59fd87d202edb1cc4f51941
tersawy <mohamedtersawy96@gmail.com>
2021-07-23 06:51:11 +0200

    Added migration Quotation table

---
+++ server/database/migrations/2021_07_23_041942_create_quotations_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Helpers\Constants;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotations', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('tax')->default(0);
+            $table->integer('discount')->default(0);
+            $table->tinyInteger("discount_method")->default(Constants::DISCOUNT_FIXED); // 0 Fixed, 1 Percent
+            $table->tinyInteger('status')->default(Constants::QUOTATION_SENT);
+            $table->integer('shipping')->default(0);
+
+            $table->text('note')->nullable();
+            $table->date('date')->default(date('Y-m-d'));
+
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('restrict');
+
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotations');
+    }
+}
