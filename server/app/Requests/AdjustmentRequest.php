4062854c047a9494823d5df908152dd4102a006a
tersawy <mohamedtersawy96@gmail.com>
2021-07-23 10:11:35 +0200

    Added AdjustmentRequest for request validation

---
+++ server/app/Requests/AdjustmentRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Requests;
+
+use App\Helpers\Constants;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class AdjustmentRequest extends ValidateRequest
+{
+
+  public static function rules()
+  {
+    $rules = [
+      'warehouse_id'           => ['required', 'numeric', 'min:1', 'exists:warehouses,id'],
+      'note'                   => ['string', 'max:255', 'nullable'],
+      'date'                   => ['required', 'string', 'max:10', 'date_format:Y-m-d'],
+      // Products Validations
+      'products'               => ['required', 'array', 'min:1'],
+      'products.*'             => ['required', 'array', 'min:1'],
+      'products.*.product_id'  => ['required', 'numeric', 'min:1', 'exists:products,id'],
+      'products.*.variant_id'  => ['numeric', 'min:1', 'exists:product_variants,id', 'nullable'],
+      'products.*.type'        => ['required', 'numeric', Rule::in(Constants::ADJUSTMENT_STATUS)],
+      'products.*.quantity'    => ['required', 'numeric', 'min:1']
+    ];
+
+    return $rules;
+  }
+
+
+  public static function validationCreate(Request $req)
+  {
+    $rules = AdjustmentRequest::rules();
+
+    return $req->validate($rules);
+  }
+
+
+  public static function validationUpdate(Request $req)
+  {
+    $req->merge(['id' => $req->route('id')]);
+
+    $rules = AdjustmentRequest::rules();
+
+    $rules['id'] = ['required', 'numeric', 'min:1'];
+
+    return $req->validate($rules);
+  }
+}
