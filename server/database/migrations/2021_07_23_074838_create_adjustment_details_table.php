97145650c88d141347bf39062910796a6ce55ab6
tersawy <mohamedtersawy96@gmail.com>
2021-07-23 10:02:13 +0200

    Added migration AdjustmentDetail table

---
+++ server/database/migrations/2021_07_23_074838_create_adjustment_details_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Helpers\Constants;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdjustmentDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adjustment_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->float('quantity');
+            $table->tinyInteger('type')->default(Constants::ADJUSTMENT_ADDITION);
+            
+            $table->bigInteger('variant_id')->nullable();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('restrict');
+
+            $table->unsignedBigInteger('adjustment_id');
+            $table->foreign('adjustment_id')->references('id')->on('adjustments')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adjustment_details');
+    }
+}
