b1e83ec569c515c603aa4e6b3490b13020891635
tersawy <mohamedtersawy96@gmail.com>
2021-07-23 10:01:57 +0200

    Added migration Adjustment table

---
+++ server/database/migrations/2021_07_23_071407_create_adjustments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdjustmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adjustments', function (Blueprint $table) {
+            $table->id();
+
+            $table->float('items_count');
+
+            $table->text('note')->nullable();
+            $table->date('date')->default(date('Y-m-d'));
+
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adjustments');
+    }
+}
