45d1aa5ba00bdea62069da85ee7acd6fc665c1cf
tersawy <mohamedtersawy96@gmail.com>
2021-07-23 14:08:24 +0200

    Added index() method to return all Adjustments records in AdjustmentController

--- server/app/Http/Controllers/AdjustmentController.php
+++ server/app/Http/Controllers/AdjustmentController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Adjustment;
 use Illuminate\Http\Request;
 
 class AdjustmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $warehouse = ['warehouse' => function ($query) {
+            $query->select(['id', 'name']);
+        }];
+
+        $purchases = Adjustment::with($warehouse)->get();
+
+        $purchases = $purchases->map(function ($purchase) {
+            return [
+                'id'            => $purchase->id,
+                'reference'     => $purchase->reference,
+                'items_count'   => $purchase->items_count,
+                'warehouse'     => $purchase->warehouse,
+                'date'          => $purchase->date
+            ];
+        });
+
+        return $this->success($purchases);
+    }
 }
