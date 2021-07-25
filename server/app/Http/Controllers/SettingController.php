a85adf46adfdd8cc4832ee349ae2afbdf5f82bdf
tersawy <mohamedtersawy96@gmail.com>
2021-07-25 18:32:55 +0200

    Added index() method to SettingController

--- server/app/Http/Controllers/SettingController.php
+++ server/app/Http/Controllers/SettingController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function index()
+    {
+        $customer = ['customer' => function ($query) {
+            $query->select(['id', 'name']);
+        }];
+
+        $warehouse = ['warehouse' => function ($query) {
+            $query->select(['id', 'name']);
+        }];
+
+        $currency = ['currency' => function ($query) {
+            $query->select(['id', 'name']);
+        }];
+
+        $with_fields = array_merge([], $customer, $warehouse, $currency);
+        
+        $settings = Setting::with($with_fields)->get()->first();
+
+        return $this->success($settings);
+    }
 }
