86ad724452957dc420dc6dcb0629971cf54c1d97
tersawy <mohamedtersawy96@gmail.com>
2021-07-25 06:29:03 +0200

    Removed unused classes

--- server/app/Http/Controllers/Auth/AuthController.php
+++ server/app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
