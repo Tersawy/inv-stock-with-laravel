23671a994836d7f2080bd53e892aab6beb49c567
tersawy <mohamedtersawy96@gmail.com>
2021-07-25 06:20:16 +0200

    Replace Constant ADJUSTMENT_STATUS to ADJUSTMENT_DETAILS_STATUS and Added status field to validation rules

--- server/app/Requests/AdjustmentRequest.php
+++ server/app/Requests/AdjustmentRequest.php
@@ -15,12 +15,13 @@ class AdjustmentRequest extends ValidateRequest
       'warehouse_id'           => ['required', 'numeric', 'min:1', 'exists:warehouses,id'],
       'note'                   => ['string', 'max:255', 'nullable'],
       'date'                   => ['required', 'string', 'max:10', 'date_format:Y-m-d'],
+      'status'                 => ['required', 'numeric', Rule::in(Constants::ADJUSTMENT_STATUS)],
       // Products Validations
       'products'               => ['required', 'array', 'min:1'],
       'products.*'             => ['required', 'array', 'min:1'],
       'products.*.product_id'  => ['required', 'numeric', 'min:1', 'exists:products,id'],
       'products.*.variant_id'  => ['numeric', 'min:1', 'exists:product_variants,id', 'nullable'],
-      'products.*.type'        => ['required', 'numeric', Rule::in(Constants::ADJUSTMENT_STATUS)],
+      'products.*.type'        => ['required', 'numeric', Rule::in(Constants::ADJUSTMENT_DETAILS_STATUS)],
       'products.*.quantity'    => ['required', 'numeric', 'min:1']
     ];
 
