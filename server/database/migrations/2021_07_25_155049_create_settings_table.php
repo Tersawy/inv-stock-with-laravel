e576772e61f4afc86524e8b786e4eaf614a7d49f
tersawy <mohamedtersawy96@gmail.com>
2021-07-25 18:01:42 +0200

    Added Setting migration table

---
+++ server/database/migrations/2021_07_25_155049_create_settings_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+			$table->string('email');
+			$table->string('company_name');
+			$table->string('company_phone');
+			$table->string('company_address');
+			$table->string('company_logo')->nullable();
+            $table->string('footer')->default('Stocky - Ultimate Inventory With POS');
+            $table->string('developed_by')->default('Stocky');
+
+            $table->unsignedBigInteger('currency_id')->nullable();
+            $table->foreign('currency_id')->references('id')->on('currencies')->onDelete('restrict');
+
+            $table->unsignedBigInteger('warehouse_id')->nullable();
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('restrict');
+
+			$table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
