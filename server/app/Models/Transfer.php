dd4975cfaf7fd7c460d9a3efe77ddb92454f7612
tersawy <mohamedtersawy96@gmail.com>
2021-07-25 10:23:23 +0200

    Added Transfer Model

---
+++ server/app/Models/Transfer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\InvoiceAttributes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transfer extends Model
+{
+    use SoftDeletes, InvoiceAttributes;
+
+    protected $fillable = [
+        'from_warehouse_id',
+        'to_warehouse_id',
+        'tax',
+        'discount',
+        'discount_method',
+        'status',
+        'shipping',
+        'note',
+        'date',
+        'items_count'
+    ];
+
+    protected $append = ['reference'];
+
+    protected $fieldActionName = 'cost';
+
+    public function details()
+    {
+        return $this->hasMany(TransferDetail::class);
+    }
+
+    public function getReferenceAttribute()
+    {
+        return 'TR_' . (1110 + $this->id);
+    }
+}
