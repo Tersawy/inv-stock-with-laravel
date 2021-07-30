e563779e3e7ea14dc37cdc87dd7e5c2891f758a4
tersawy <mohamedtersawy96@gmail.com>
2021-07-30 16:18:34 +0200

    Added pagination to products and get image path in show() method

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -17,7 +17,16 @@ class ProductController extends Controller
 {
   use ProductWarehouseOperations;
 
-  public function index()
+  protected $filterationFields = [
+    'name'      => 'name',
+    'category'  => 'category_id',
+    'brand'     => 'brand_id',
+    'code'      => 'code'
+  ];
+
+  protected $searchFields = ['name', 'code'];
+
+  public function index(Request $req)
   {
     $category = ['category' => function ($query) {
       $query->select(['id', 'name']);
@@ -35,11 +44,15 @@ class ProductController extends Controller
       $query->select(['name', 'product_id']);
     }];
 
-    $withFields = array_merge([], $category, $brand, $unit, $images);
+    $with_fields = array_merge($category, $brand, $unit, $images);
 
-    $products = Product::with($withFields)->get(['id', 'name', 'code', 'price', 'main_unit_id', 'brand_id', 'category_id']);
+    $query = Product::query();
 
-    $products = $products->map(function ($product) {
+    $this->handleQuery($req, $query);
+
+    $products = $query->with($with_fields)->select(['id', 'name', 'code', 'price', 'main_unit_id', 'brand_id', 'category_id'])->paginate($req->per_page);
+
+    $products->getCollection()->transform(function ($product) {
       return [
         'id'        => $product->id,
         'name'      => $product->name,
@@ -67,9 +80,9 @@ class ProductController extends Controller
       $query->select(['name', 'product_id']);
     }];
 
-    $withFields = array_merge([], $variants, $images);
+    $with_fields = array_merge($variants, $images);
 
-    $products = Product::with($withFields)->get(['id', 'name', 'code', 'has_variants']);
+    $products = Product::with($with_fields)->get(['id', 'name', 'code', 'has_variants']);
 
     $newProducts = [];
 
@@ -92,7 +105,6 @@ class ProductController extends Controller
       }
     }
 
-    // return $this->success($products);
     return $this->success($newProducts);
   }
 
@@ -111,11 +123,11 @@ class ProductController extends Controller
       $query->select(['id', 'short_name', 'value', 'operator']);
     }];
 
-    $withFields = array_merge([], $saleUnit, $purchaseUnit);
+    $with_fields = array_merge($saleUnit, $purchaseUnit);
 
     $columns = ['id', 'purchase_unit_id', 'sale_unit_id', 'cost', 'price', 'tax', 'tax_method', 'instock'];
 
-    $product = Product::with($withFields)->find($req->id, $columns);
+    $product = Product::with($with_fields)->find($req->id, $columns);
 
     return $this->success($product->details());
   }
@@ -135,9 +147,11 @@ class ProductController extends Controller
       $query->select(['name', 'product_id']);
     }];
 
-    $withFields = array_merge([], $images, $variants);
+    $with_fields = array_merge($images, $variants);
 
-    $product = Product::where('id', $req->id)->with($withFields)->get()->first();
+    $product = Product::where('id', $req->id)->with($with_fields)->get()->first();
+
+    $product->path = $product->path;
 
     return $this->success($product);
   }
