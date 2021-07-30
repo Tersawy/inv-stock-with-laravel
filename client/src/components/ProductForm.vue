b35a5e163b9c2e5a19a1cd1bcf33aebaaba683e3
tersawy <mohamedtersawy96@gmail.com>
2021-07-30 07:48:04 +0200

    Refactoring ProductForm and added bootstrap input tags

--- client/src/components/ProductForm.vue
+++ client/src/components/ProductForm.vue
@@ -1,131 +1,164 @@
 <template>
-	<div class="product-form mt-5">
+	<div class="product-form py-3">
 		<b-container fluid>
-			<b-form @submit.prevent="handleSave">
-				<b-row>
-					<b-col cols="8">
-						<b-row cols="2">
-							<b-col>
-								<b-form-group label="Product Name" label-for="name">
-									<b-form-input id="name" placeholder="Enter Product Name" v-model="product.name" />
-									<InputError field="name" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Barcode Symbology" label-for="barcode_type">
-									<b-select id="barcode_type" v-model="product.barcode_type" :options="barcodeOpt" @change="generateCode" />
-									<InputError field="barcode_type" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Code" label-for="code">
-									<b-input-group>
-										<b-form-input id="code" placeholder="code" v-model="product.code" disabled />
-										<b-input-group-prepend is-text>
-											<span class="mb-0 c-pointer" @click="generateCode">
-												<b-icon icon="upc"></b-icon>
-											</span>
-										</b-input-group-prepend>
-									</b-input-group>
-									<InputError field="code" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Price" label-for="price">
-									<b-form-input id="price" type="number" placeholder="Enter Product Price" v-model.number="product.price" />
-									<InputError field="price" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Cost" label-for="cost">
-									<b-form-input id="cost" type="number" placeholder="Enter Product Cost" v-model.number="product.cost" />
-									<InputError field="cost" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Minimum Alert" label-for="minimum">
-									<b-form-input
-										id="minimum"
-										type="number"
-										placeholder="Enter Product Minimum Alert"
-										v-model.number="product.minimum"
-									/>
-									<InputError field="minimum" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Order Tax" label-for="tax">
-									<b-input-group append="%">
-										<b-form-input id="tax" placeholder="tax" v-model.number="product.tax" />
-									</b-input-group>
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Tax Type" label-for="tax_method">
-									<b-select id="tax_method" v-model.number="product.tax_method" :options="taxMethodOpt" />
-									<InputError field="tax_method" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Category" label-for="category_id">
-									<b-select id="category_id" v-model.number="product.category_id" :options="categoriesOpt" />
-									<InputError field="category_id" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Brand" label-for="brand">
-									<b-select id="brand" v-model.number="product.brand_id" :options="brandsOpt" />
-									<InputError field="brand" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Product Unit" label-for="main_unit_id">
-									<b-select id="main_unit_id" v-model.number="product.main_unit_id" :options="unitsOpt" @change="setSubUnits" />
-									<InputError field="main_unit_id" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Purchase Unit" label-for="purchase_unit">
-									<b-select id="purchase_unit" v-model.number="product.purchase_unit_id" :options="purchaseUnitsOpt" />
-									<InputError field="purchase_unit" />
-								</b-form-group>
-							</b-col>
-							<b-col>
-								<b-form-group label="Sale Unit" label-for="sale_unit">
-									<b-select id="sale_unit" v-model.number="product.sale_unit_id" :options="saleUnitsOpt" />
-									<InputError field="sale_unit" />
-								</b-form-group>
-							</b-col>
-						</b-row>
-					</b-col>
-					<b-col>
-						<b-card header="Upload product images">
-							<VueUploadMultipleImage
-								class="d-flex flex-column align-items-center"
-								browseText="(or) Select"
-								dragText="Drag & Drop Multiple images For product"
-								primaryText="Success"
-								:data-images="images"
-								@upload-success="uploadImageSuccess"
-								@before-remove="beforeRemoveImage"
-								:showPrimary="false"
-								:showEdit="false"
-								accept="image/jpeg,image/png,image/jpg"
-							/>
-							<b-form-checkbox v-model.number="product.has_variants" class="mt-3"> Product has varinats </b-form-checkbox>
-							<vue-tags-input
-								class="mt-3"
-								v-model="variant"
-								:tags="variants"
-								placeholder="add variant"
-								@tags-changed="(newTags) => (variants = newTags)"
-								v-if="product.has_variants"
-							/>
+			<b-row>
+				<b-col cols="12" class="pb-3">
+					<b-form @submit.prevent="handleSave">
+						<b-card header="Product details">
+							<b-row cols="3">
+								<b-col>
+									<b-form-group label="Product Name" label-for="name">
+										<b-form-input id="name" placeholder="Enter Product Name" v-model="product.name" />
+										<InputError field="name" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Barcode Symbology" label-for="barcode_type">
+										<b-select id="barcode_type" v-model="product.barcode_type" :options="barcodeOpt" @change="generateCode" />
+										<InputError field="barcode_type" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Code" label-for="code">
+										<b-input-group>
+											<b-form-input id="code" placeholder="code" v-model="product.code" disabled />
+											<b-input-group-prepend is-text>
+												<span class="mb-0 c-pointer" @click="generateCode">
+													<b-icon icon="upc"></b-icon>
+												</span>
+											</b-input-group-prepend>
+										</b-input-group>
+										<InputError field="code" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Price" label-for="price">
+										<b-form-input id="price" type="number" placeholder="Enter Product Price" v-model.number="product.price" />
+										<InputError field="price" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Cost" label-for="cost">
+										<b-form-input id="cost" type="number" placeholder="Enter Product Cost" v-model.number="product.cost" />
+										<InputError field="cost" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Minimum Alert" label-for="minimum">
+										<b-form-input
+											id="minimum"
+											type="number"
+											placeholder="Enter Product Minimum Alert"
+											v-model.number="product.minimum"
+										/>
+										<InputError field="minimum" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Order Tax" label-for="tax">
+										<b-input-group append="%">
+											<b-form-input id="tax" placeholder="tax" v-model.number="product.tax" />
+										</b-input-group>
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Tax Type" label-for="tax_method">
+										<b-select id="tax_method" v-model.number="product.tax_method" :options="taxMethodOpt" />
+										<InputError field="tax_method" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Category" label-for="category_id">
+										<b-select id="category_id" v-model.number="product.category_id" :options="categoriesOpt" />
+										<InputError field="category_id" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Brand" label-for="brand">
+										<b-select id="brand" v-model.number="product.brand_id" :options="brandsOpt" />
+										<InputError field="brand" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Product Unit" label-for="main_unit_id">
+										<b-select id="main_unit_id" v-model.number="product.main_unit_id" :options="unitsOpt" @change="setSubUnits" />
+										<InputError field="main_unit_id" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Purchase Unit" label-for="purchase_unit">
+										<b-select id="purchase_unit" v-model.number="product.purchase_unit_id" :options="purchaseUnitsOpt" />
+										<InputError field="purchase_unit" />
+									</b-form-group>
+								</b-col>
+								<b-col>
+									<b-form-group label="Sale Unit" label-for="sale_unit">
+										<b-select id="sale_unit" v-model.number="product.sale_unit_id" :options="saleUnitsOpt" />
+										<InputError field="sale_unit" />
+									</b-form-group>
+								</b-col>
+							</b-row>
 						</b-card>
-					</b-col>
-				</b-row>
-				<b-btn :variant="`${isUpdate ? 'success' : 'primary'}`" type="submit">save</b-btn>
-			</b-form>
+					</b-form>
+				</b-col>
+				<b-col cols="6" class="py-3">
+					<b-card header="Upload product images">
+						<VueUploadMultipleImage
+							class="d-flex flex-column align-items-center"
+							browseText="(or) Select"
+							dragText="Drag & Drop Multiple images For product"
+							primaryText="Success"
+							:data-images="images"
+							@upload-success="uploadImageSuccess"
+							@before-remove="beforeRemoveImage"
+							:showPrimary="false"
+							:showEdit="false"
+							accept="image/jpeg,image/png,image/jpg"
+						/>
+					</b-card>
+				</b-col>
+				<b-col cols="6" class="py-3" v-if="!isUpdate">
+					<b-card header="Product variants" class="h-100">
+						<b-form-tags
+							v-model="variants"
+							no-outer-focus
+							class="mb-2 border-0"
+							tagVariant="success"
+							:tag-validator="tagValidator"
+						>
+							<template v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag }">
+								<b-input-group class="mb-2">
+									<b-form-input
+										v-bind="inputAttrs"
+										v-on="inputHandlers"
+										placeholder="Enter Variant Name"
+										id="variantName"
+									></b-form-input>
+									<b-input-group-append>
+										<b-button @click="addTag()" variant="outline-success">Add</b-button>
+									</b-input-group-append>
+								</b-input-group>
+								<div id="tags-validation-help" class="small text-muted">
+									Product variant must be 3 to 54 characters in length.
+								</div>
+								<div class="d-block font-d mt-1">
+									<b-form-tag
+										v-for="tag in tags"
+										@remove="removeTag(tag)"
+										:key="tag"
+										:title="tag"
+										:variant="tagVariant"
+										class="mr-1"
+									>
+										{{ tag }}
+									</b-form-tag>
+								</div>
+							</template>
+						</b-form-tags>
+					</b-card>
+				</b-col>
+			</b-row>
+			<b-btn :variant="`${isUpdate ? 'success' : 'primary'}`" type="submit" @click="handleSave">save</b-btn>
 		</b-container>
 	</div>
 </template>
@@ -154,7 +187,7 @@
 				tax_method: 0,
 				note: null,
 				category_id: null,
-				brand_id: 0,
+				brand_id: null,
 				main_unit_id: null,
 				purchase_unit_id: null,
 				sale_unit_id: null,
@@ -177,8 +210,7 @@
 				{ text: "Exclusive", value: 0, disabled: false }
 			],
 			images: [],
-			variants: [],
-			variant: ""
+			variants: []
 		}),
 
 		async mounted() {
@@ -250,6 +282,10 @@
 				this.saleUnitsOpt = [this.saleUnitsOpt[0], ...mainUnit.sub_units];
 			},
 
+			tagValidator(tag) {
+				return tag.length > 2 && tag.length < 54;
+			},
+
 			generateCode() {
 				this.product.barcode_type = this.product.barcode_type || this.barcodeOpt[1].value;
 
@@ -291,7 +327,7 @@
 					formData.append("has_variants", true);
 
 					this.variants.forEach((variant, i) => {
-						formData.append(`variants[${i}][name]`, variant.text);
+						formData.append(`variants[${i}][name]`, variant);
 					});
 				}
 
@@ -313,3 +349,14 @@
 		}
 	};
 </script>
+
+<style scoped lang="scss">
+	.product-form {
+		#variantName {
+			&:focus,
+			&.focus {
+				border-color: lighten(#28a745, 25%);
+			}
+		}
+	}
+</style>
