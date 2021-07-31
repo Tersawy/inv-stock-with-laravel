35ff7023ffd57ae95f362daf56b39d45d055a43d
tersawy <mohamedtersawy96@gmail.com>
2021-07-31 14:28:22 +0200

    Added Trait PaginationAndSearchOperation

--- server/app/Http/Controllers/Controller.php
+++ server/app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ApiResponser;
+use App\Traits\PaginationAndSearchOperation;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +11,5 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponser;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponser, PaginationAndSearchOperation;
 }
