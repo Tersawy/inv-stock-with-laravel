cacdebed6c7de8fa603b349cdada3ae16dcecfb3
tersawy <mohamedtersawy96@gmail.com>
2021-08-03 13:06:27 +0200

    Update unit validation to ProductRequest

--- server/app/Requests/ProductRequest.php
+++ server/app/Requests/ProductRequest.php
@@ -22,11 +22,25 @@ class ProductRequest extends ValidateRequest
       'note'              => ['string', 'max:255', 'nullable'],
       'category_id'       => ['required', 'numeric', 'min:1', 'exists:categories,id'],
       'brand_id'          => ['exclude_if:brand_id,0', 'required', 'numeric', 'min:1', 'exists:brands,id'],
-      'unit_id'           => ['required', 'numeric', 'min:1', Rule::exists('units', 'id')->where('main_unit_id', null)],
-      'purchase_unit_id'  => ['required', 'numeric', 'min:1', Rule::exists('units', 'id')->where('main_unit_id', $req->get('unit_id'))],
-      'sale_unit_id'      => ['required', 'numeric', 'min:1', Rule::exists('units', 'id')->where('main_unit_id', $req->get('unit_id'))],
       'has_variants'      => ['required', 'boolean'],
       'has_images'        => ['required', 'boolean'],
+      'unit_id'           => ['required', 'numeric', 'min:1', Rule::exists('units', 'id')->whereNull('main_unit_id')],
+      'purchase_unit_id'  => [
+        'required',
+        'numeric',
+        'min:1',
+        Rule::exists('units', 'id')->where(function ($query) use ($req) {
+          $query->where('main_unit_id', $req->get('unit_id'))->orWhere('id', $req->get('unit_id'));
+        })
+      ],
+      'sale_unit_id'      => [
+        'required',
+        'numeric',
+        'min:1',
+        Rule::exists('units', 'id')->where(function ($query) use ($req) {
+          $query->where('main_unit_id', $req->get('unit_id'))->orWhere('id', $req->get('unit_id'));
+        })
+      ]
     ];
 
     $variants_rules = [
