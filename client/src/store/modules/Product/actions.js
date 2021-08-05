e63729523592a15ded3fcbe8fc89dc2810c6b535
tersawy <mohamedtersawy96@gmail.com>
2021-08-05 23:33:35 +0200

    Added details action to get product details for invoices

--- client/src/store/modules/Product/actions.js
+++ client/src/store/modules/Product/actions.js
@@ -1,1 +1,10 @@
-export default {};
+import api from "@/plugins/api";
+
+export default {
+	details: ({ commit, state }, item) => {
+		return api("post", `${state.prefix}/details`, item, (err, res) => {
+			if (err) return;
+			commit("details", res);
+		});
+	}
+};
