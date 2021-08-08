9a4032dc62a8a6d0350a4aff3966edf9cf4ecfe3
tersawy <mohamedtersawy96@gmail.com>
2021-08-08 20:34:56 +0200

    Update error mixin to use error by namespace

--- client/src/mixins/errorMixin.js
+++ client/src/mixins/errorMixin.js
@@ -1,13 +1,41 @@
-import { mapMutations, mapState } from "vuex";
-
 export default {
 	computed: {
-		...mapState({
-			errors: (state) => state.errors
-		})
+		errors() {
+			this.$store.state[this.namespace].errors;
+		}
 	},
 
 	methods: {
-		...mapMutations(["setError", "removeError", "removeErrors"])
+		/**
+			@param obj e.g. { field: "fieldName", msg: "Message" }
+		*/
+		setError(obj) {
+			this.$store.commit(this.namespace + "/setError", obj);
+		},
+
+		/**
+			@param field string
+		*/
+		removeError(field) {
+			this.$store.commit(this.namespace + "/removeError", field);
+		},
+
+		/**
+			@param obj e.g. { errors: { field: ["error msg 1", "error msg 2"] }, message: "Global Msg" }
+		*/
+		setErrors(obj) {
+			this.$store.commit(this.namespace + "/removeError", obj);
+		},
+
+		removeErrors() {
+			this.$store.commit(this.namespace + "/removeError");
+		},
+
+		/**
+			@param message string
+		*/
+		setGlobalError(message) {
+			this.$store.commit("setError", message);
+		}
 	}
 };
