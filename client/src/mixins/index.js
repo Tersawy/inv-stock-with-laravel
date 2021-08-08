77bcd03cd5628c7b01bf00bbd10e09fd39a296d1
tersawy <mohamedtersawy96@gmail.com>
2021-08-08 21:25:55 +0200

    Export invoice from mixins/index

--- client/src/mixins/index.js
+++ client/src/mixins/index.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import userMixin from "@/mixins/userMixin";
 import errorMixin from "@/mixins/errorMixin";
 import formMixin from "@/mixins/formMixin";
+import invoiceMixin from "@/mixins/invoiceMixin";
 
 let runGlobalMixins = () => {
 	Vue.mixin(userMixin);
 	Vue.mixin(errorMixin);
 };
 
-export { runGlobalMixins, formMixin };
+export { runGlobalMixins, formMixin, invoiceMixin };
