ab151c9490265ebd0db661066d306afec2b0bfec
tersawy <mohamedtersawy96@gmail.com>
2021-08-08 20:33:30 +0200

    Added Invoice Mixin

---
+++ client/src/mixins/invoiceMixin.js
@@ -0,0 +1,244 @@
+import DiscountInput from "@/components/ui/inputs/DiscountInput";
+import TaxInput from "@/components/ui/inputs/TaxInput.vue";
+import ShippingInput from "@/components/ui/inputs/ShippingInput.vue";
+import SupplierInput from "@/components/ui/inputs/SupplierInput.vue";
+import WarehouseInput from "@/components/ui/inputs/WarehouseInput.vue";
+import DateInput from "@/components/ui/inputs/DateInput.vue";
+import NoteInput from "@/components/ui/inputs/NoteInput.vue";
+import InvoiceStatusInput from "@/components/ui/inputs/InvoiceStatusInput.vue";
+import InvoiceDetailsTable from "@/components/InvoiceProductsDetailsTable.vue";
+import InvoiceDetails from "@/components/InvoiceDetails.vue";
+import ProductsAutoComplete from "@/components/ProductsAutoComplete.vue";
+
+import { mapActions } from "vuex";
+
+import { DISCOUNT_FIXED, DISCOUNT_PERCENT, TAX_INCLUSIVE } from "@/helpers/constants";
+
+import { required, numeric, minLength, maxLength, minValue } from "vuelidate/lib/validators";
+import errorMixin from "./errorMixin";
+
+export default {
+	components: {
+		DiscountInput,
+		TaxInput,
+		ShippingInput,
+		SupplierInput,
+		WarehouseInput,
+		DateInput,
+		NoteInput,
+		InvoiceStatusInput,
+		InvoiceDetailsTable,
+		InvoiceDetails,
+		ProductsAutoComplete
+	},
+
+	mixins: [errorMixin],
+
+	data() {
+		let zeroFill = (v) => (+v < 10 ? `0${v}` : v);
+
+		let [m, d, y] = new Date().toLocaleDateString().split("/");
+
+		const today = `${y}-${zeroFill(m)}-${zeroFill(d)}`;
+
+		let invoice = {
+			warehouse_id: null,
+			tax: 0,
+			discount: 0,
+			discount_method: DISCOUNT_FIXED,
+			status: 0,
+			shipping: 0,
+			note: null,
+			date: today,
+			total_price: 0,
+			products: []
+		};
+
+		if (this.isPrice) {
+			invoice.customer_id = null;
+		} else {
+			invoice.supplier_id = null;
+		}
+
+		return { invoice };
+	},
+
+	validations() {
+		let invoice = {
+			warehouse_id: { required, numeric, minValue: minValue(1) },
+			tax: { numeric, minValue: minValue(0) },
+			discount: { numeric, minValue: minValue(0) },
+			discount_method: { numeric, minValue: minValue(0) },
+			status: { numeric, minValue: minValue(0) },
+			shipping: { numeric, minValue: minValue(0) },
+			note: { maxLength: maxLength(255) },
+			date: { required },
+			products: { required, minLength: minLength(1) },
+			total_price: { required, minValue: minValue(1) }
+		};
+
+		if (this.isPrice) {
+			invoice.customer_id = { required, numeric, minValue: minValue(1) };
+		} else {
+			invoice.supplier_id = { required, numeric, minValue: minValue(1) };
+		}
+
+		return { invoice, name: { required, numeric, minValue: minValue(1) } };
+	},
+
+	async mounted() {
+		this.getProductsOpt();
+		this.getWarehousesOpt();
+
+		if (this.isPrice) {
+			this.getCustomersOpt();
+		} else {
+			this.getSuppliersOpt();
+		}
+
+		if (this.invoiceIdParam) {
+			await this.getInvoice(invoiceIdParam);
+			this.invoice = { ...this.oldInvoice };
+		}
+	},
+
+	computed: {
+		oldInvoice() {
+			return this.$store.state[this.namespace].one;
+		},
+
+		isPrice() {
+			return this.invoiceFieldName == "price";
+		},
+
+		invoiceIdParam() {
+			return this.$route.params.invoiceId;
+		},
+
+		DISCOUNT_FIXED() {
+			return DISCOUNT_FIXED;
+		},
+
+		DISCOUNT_PERCENT() {
+			return DISCOUNT_PERCENT;
+		},
+
+		TAX_INCLUSIVE() {
+			return TAX_INCLUSIVE;
+		}
+	},
+
+	methods: {
+		...mapActions({
+			getWarehousesOpt: "Warehouse/options",
+			getProductsOpt: "Product/options",
+			getSuppliersOpt: "Supplier/options",
+			getCustomersOpt: "Customer/options"
+		}),
+
+		getInvoice(invoiceId) {
+			this.$store.dispatch(`${this.namespace}/one`, invoiceId);
+		},
+
+		create(invoice) {
+			this.$store.dispatch(`${this.namespace}/create`, invoice);
+		},
+
+		update(invoice) {
+			this.$store.dispatch(`${this.namespace}/update`, invoice);
+		},
+
+		net(product) {
+			let { unit_cost, unit_price, tax, tax_method, discount = 0, discount_method = DISCOUNT_FIXED, quantity = 1 } = product;
+
+			let netCost, netPrice, taxCost, taxPrice, discountCost, discountPrice, totalCost, totalPrice, subtotalCost, subtotalPrice;
+
+			discountCost = discount_method == DISCOUNT_FIXED ? discount : discount * (unit_cost / 100);
+			discountPrice = discount_method == DISCOUNT_FIXED ? discount : discount * (unit_price / 100);
+
+			let costExcludingDiscount = unit_cost - discountCost,
+				priceExcludingDiscount = unit_price - discountPrice;
+
+			if (tax_method == TAX_INCLUSIVE) {
+				let taxDivide = 1 + tax / 100;
+
+				netCost = costExcludingDiscount / taxDivide;
+
+				netPrice = priceExcludingDiscount / taxDivide;
+
+				taxCost = costExcludingDiscount - netCost;
+
+				taxPrice = priceExcludingDiscount - netPrice;
+			} else {
+				netCost = costExcludingDiscount;
+
+				netPrice = priceExcludingDiscount;
+
+				taxCost = tax * (costExcludingDiscount / 100);
+
+				taxPrice = tax * (priceExcludingDiscount / 100);
+			}
+
+			totalCost = netCost + taxCost;
+			totalPrice = netPrice + taxPrice;
+
+			subtotalCost = quantity * totalCost;
+			subtotalPrice = quantity * totalPrice;
+
+			return {
+				cost: netCost,
+				price: netPrice,
+				taxCost,
+				taxPrice,
+				discountCost,
+				discountPrice,
+				totalCost,
+				totalPrice,
+				subtotalCost,
+				subtotalPrice
+			};
+		},
+
+		handleSave() {
+			let invoice = Object.assign({}, this.invoice);
+
+			invoice.products = invoice.products.map((selected) => {
+				let product = {
+					quantity: selected.quantity,
+					tax: selected.tax,
+					tax_method: selected.tax_method,
+					discount: selected.discount,
+					discount_method: selected.discount_method,
+					product_id: selected.id,
+					variant_id: selected.variant_id
+				};
+
+				if (this.isPrice) {
+					product.price = selected.unit_price;
+				} else {
+					product.cost = selected.unit_cost;
+				}
+
+				return product;
+			});
+
+			invoice.total_price = this.invoiceTotalPrice;
+
+			this.$v.$touch();
+
+			if (this.$v.$invalid) return;
+
+			if (this.invoiceIdParam) return this.handleUpdate();
+
+			return this.handleCreate();
+		},
+
+		async handleCreate() {
+			await this.create(this.invoice);
+		},
+
+		async handleUpdate() {
+			await this.update(this.invoice);
+		}
+	}
+};
