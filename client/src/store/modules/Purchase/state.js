38ac2d9f0d10ea4aa6b04af9cfa3c603d7b5dd18
tersawy <mohamedtersawy96@gmail.com>
2021-08-08 20:42:41 +0200

    Added purchase options

--- client/src/store/modules/Purchase/state.js
+++ client/src/store/modules/Purchase/state.js
@@ -1,3 +1,10 @@
+import { PURCHASE_RECEIVED, PURCHASE_PENDING, PURCHASE_ORDERED } from "@/helpers/constants";
+
 export default {
-	prefix: "purchase"
+	prefix: "purchase",
+	options: [
+		{ text: "Received", value: PURCHASE_RECEIVED },
+		{ text: "Pending", value: PURCHASE_PENDING },
+		{ text: "Ordered", value: PURCHASE_ORDERED }
+	]
 };
