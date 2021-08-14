db0f9f2b486ab0acd58662fb7676c5da8fd5844c
tersawy <mohamedtersawy96@gmail.com>
2021-08-14 18:32:41 +0200

    Nothing !

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SpaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function() {
+Route::get('/login', function () {
     return "you're welcome";
 })->name("login");
 
