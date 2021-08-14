a7beae84e3a1d12b47f4f215a1338d8ff8b0672a
tersawy <mohamedtersawy96@gmail.com>
2021-08-14 21:22:43 +0200

    Added update product method to Product store module

--- client/src/store/modules/Product/actions.js
+++ client/src/store/modules/Product/actions.js
@@ -1,23 +1,33 @@
 import api from "@/plugins/api";
 
 export default {
-	details: ({ commit, state }, item) => {
+	details({ commit, state }, item) {
 		return api("post", `${state.prefix}/details`, item, (err, res) => {
 			if (err) return;
 			commit("details", res);
 		});
 	},
 
-	create: ({ state, dispatch }, item) => {
+	create({ state, dispatch }, item) {
 		return api(
 			"post",
 			`${state.prefix}/create`,
 			item,
-			(err) => {
-				if (err) return;
+			(err, res) => {
+				if (err) return Promise.reject(err);
 				dispatch("all");
+				return Promise.resolve(res);
 			},
 			true
 		);
+	},
+
+	update({ state, dispatch }, item) {
+		item.set("_method", "put");
+		return api("post", `${state.prefix}/${item.get("id")}`, item, (err, res) => {
+			if (err) return Promise.reject(err);
+			dispatch("all");
+			return Promise.resolve(res);
+		});
 	}
 };
