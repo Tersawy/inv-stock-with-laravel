21a67472dda6ad99abcd95603ed15617580b3893
tersawy <mohamedtersawy96@gmail.com>
2021-08-14 20:16:24 +0200

    Remove hooks cause when second param is a function that's the same behavior on bind and update

--- client/src/directives.js
+++ client/src/directives.js
@@ -30,210 +30,182 @@ import {
 	TRANSFER_STATUS_STR
 } from "@/helpers/constants";
 
-Vue.directive("payment-status", {
-	bind(el, binding) {
-		let v = binding.value;
+Vue.directive("payment-status", function (el, binding) {
+	let v = binding.value;
 
-		let variant = () => {
-			switch (v) {
-				case PAYMENT_STATUS_PAID:
-					return "success";
-				case PAYMENT_STATUS_UNPAID:
-					return "danger";
-				case PAYMENT_STATUS_PARTIAL:
-					return "warning";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${PAYMENT_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("purchase-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case PURCHASE_RECEIVED:
-					return "success";
-				case PURCHASE_PENDING:
-					return "danger";
-				case PURCHASE_ORDERED:
-					return "warning";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${PURCHASE_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("purchase-return-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case PURCHASE_RETURN_COMPLETED:
-					return "success";
-				case PURCHASE_RETURN_PENDING:
-					return "danger";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${PURCHASE_RETURN_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("sale-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case SALE_COMPLETED:
-					return "success";
-				case SALE_PENDING:
-					return "danger";
-				case SALE_ORDERED:
-					return "warning";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${SALE_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("sale-return-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case SALE_RETURN_RECEIVED:
-					return "success";
-				case SALE_RETURN_PENDING:
-					return "danger";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${SALE_RETURN_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("quotation-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case QUOTATION_SENT:
-					return "success";
-				case QUOTATION_PENDING:
-					return "danger";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${QUOTATION_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("adjustment-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case ADJUSTMENT_APPROVED:
-					return "success";
-				case ADJUSTMENT_NOT_APPROVED:
-					return "danger";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${ADJUSTMENT_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("transfer-status", {
-	bind(el, binding) {
-		let v = binding.value;
-
-		let variant = () => {
-			switch (v) {
-				case TRANSFER_COMPLETED:
-					return "success";
-				case TRANSFER_PENDING:
-					return "danger";
-				case TRANSFER_SENT:
-					return "warning";
-				default:
-					return "info";
-			}
-		};
-
-		let badge = `<span class='badge badge-outline-${variant()}'>${TRANSFER_STATUS_STR[v]}</span>`;
-
-		el.innerHTML = badge;
-	}
-});
-
-Vue.directive("relation", {
-	bind(el, binding) {
-		let v = binding.value,
-			m = binding.modifiers,
-			mKeys = Object.keys(m);
-
-		if (!v) {
-			el.classList.add("text-muted");
-			el.textContent = "Unknown";
-			return;
+	let variant = () => {
+		switch (v) {
+			case PAYMENT_STATUS_PAID:
+				return "success";
+			case PAYMENT_STATUS_UNPAID:
+				return "danger";
+			case PAYMENT_STATUS_PARTIAL:
+				return "warning";
+			default:
+				return "info";
 		}
+	};
 
-		if (typeof v === "object") {
-			if (mKeys.length && typeof v[mKeys[0]] !== "undefined") {
-				el.textContent = v[mKeys[0]];
-			} else {
-				el.textContent = v.name;
-			}
-		}
-	}
+	let badge = `<span class='badge badge-outline-${variant()}'>${PAYMENT_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
 });
 
-Vue.directive("floating", {
-	bind(el, binding) {
-		let v = binding.value,
-			m = binding.modifiers,
-			mKeys = Object.keys(m);
+Vue.directive("purchase-status", function (el, binding) {
+	let v = binding.value;
 
-		el.textContent = v.toFixed(mKeys[0] || 2);
+	let variant = () => {
+		switch (v) {
+			case PURCHASE_RECEIVED:
+				return "success";
+			case PURCHASE_PENDING:
+				return "danger";
+			case PURCHASE_ORDERED:
+				return "warning";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${PURCHASE_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("purchase-return-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case PURCHASE_RETURN_COMPLETED:
+				return "success";
+			case PURCHASE_RETURN_PENDING:
+				return "danger";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${PURCHASE_RETURN_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("sale-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case SALE_COMPLETED:
+				return "success";
+			case SALE_PENDING:
+				return "danger";
+			case SALE_ORDERED:
+				return "warning";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${SALE_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("sale-return-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case SALE_RETURN_RECEIVED:
+				return "success";
+			case SALE_RETURN_PENDING:
+				return "danger";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${SALE_RETURN_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("quotation-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case QUOTATION_SENT:
+				return "success";
+			case QUOTATION_PENDING:
+				return "danger";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${QUOTATION_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("adjustment-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case ADJUSTMENT_APPROVED:
+				return "success";
+			case ADJUSTMENT_NOT_APPROVED:
+				return "danger";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${ADJUSTMENT_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("transfer-status", function (el, binding) {
+	let v = binding.value;
+
+	let variant = () => {
+		switch (v) {
+			case TRANSFER_COMPLETED:
+				return "success";
+			case TRANSFER_PENDING:
+				return "danger";
+			case TRANSFER_SENT:
+				return "warning";
+			default:
+				return "info";
+		}
+	};
+
+	let badge = `<span class='badge badge-outline-${variant()}'>${TRANSFER_STATUS_STR[v]}</span>`;
+
+	el.innerHTML = badge;
+});
+
+Vue.directive("relation", function (el, binding) {
+	let v = binding.value,
+		m = binding.modifiers,
+		mKeys = Object.keys(m);
+
+	if (!v) {
+		el.classList.add("text-muted");
+		el.textContent = "Unknown";
+		return;
+	}
+
+	if (typeof v === "object") {
+		if (mKeys.length && typeof v[mKeys[0]] !== "undefined") {
+			el.textContent = v[mKeys[0]];
+		} else {
+			el.textContent = v.name;
+		}
 	}
 });
