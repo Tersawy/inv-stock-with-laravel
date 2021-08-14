8c43ac00fb3547e482f532beaf86c285053956e5
tersawy <mohamedtersawy96@gmail.com>
2021-08-14 21:25:26 +0200

    Added Price, Cost, Customer and TaxMethods Inputs

---
+++ client/src/components/ui/inputs/TaxMethodsInput.vue
@@ -0,0 +1,16 @@
+<template>
+	<b-form-group label="Tax Type" label-for="tax_method">
+		<b-select id="tax_method" v-model.number="object.tax_method" :options="taxMethods" />
+	</b-form-group>
+</template>
+
+<script>
+	export default {
+		props: ["object"],
+		computed: {
+			taxMethods() {
+				return this.$store.state.taxMethods;
+			}
+		}
+	};
+</script>
