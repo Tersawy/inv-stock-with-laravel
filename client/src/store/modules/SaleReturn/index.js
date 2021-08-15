a46f015dd5de16dceb760e753108a79838269780
tersawy <mohamedtersawy96@gmail.com>
2021-08-15 19:40:17 +0200

    Fix shareded invoices module directory

--- client/src/store/modules/SaleReturn/index.js
+++ client/src/store/modules/SaleReturn/index.js
@@ -4,9 +4,9 @@ import actions from "./actions";
 import sharededState from "@/store/shareded/all/state";
 import sharededMutations from "@/store/shareded/all/mutations";
 import sharededActions from "@/store/shareded/all/actions";
-import invoicesState from "@/store/invoices/all/state";
-import invoicesMutations from "@/store/invoices/all/mutations";
-import invoicesActions from "@/store/invoices/all/actions";
+import invoicesState from "@/store/shareded/invoices/state";
+import invoicesMutations from "@/store/shareded/invoices/mutations";
+import invoicesActions from "@/store/shareded/invoices/actions";
 
 export default {
 	state: { ...sharededState, ...invoicesState, ...state },
