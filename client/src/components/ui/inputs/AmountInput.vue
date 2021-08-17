e0c1c8d094ddedbc98f6965db462798fad56635a
tersawy <mohamedtersawy96@gmail.com>
2021-08-17 17:58:32 +0200

    Added input props for Amount input

--- client/src/components/ui/inputs/AmountInput.vue
+++ client/src/components/ui/inputs/AmountInput.vue
@@ -1,13 +1,13 @@
 <template>
 	<b-form-group label="Amount" label-for="amount">
 		<b-input-group append="$">
-			<b-form-input type="number" min="0" id="amount" placeholder="amount" v-model.number="object.amount" />
+			<b-form-input v-bind="inputProps" type="number" min="0" id="amount" placeholder="amount" v-model.number="object.amount" />
 		</b-input-group>
 	</b-form-group>
 </template>
 
 <script>
 	export default {
-		props: ["object"]
+		props: ["object", "inputProps"]
 	};
 </script>
