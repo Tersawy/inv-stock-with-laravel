6a7b5a7109d5107db9f95ba8e4b49dbf92fdfa6b
tersawy <mohamedtersawy96@gmail.com>
2021-08-18 12:33:39 +0200

    Remove trashed routes

--- server/routes/api/warehouse.php
+++ server/routes/api/warehouse.php
@@ -8,17 +8,11 @@ Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'warehouse'], functi
 
     Route::get('/options', [WarehouseController::class, 'options']);
 
-    Route::get('/trashed', [WarehouseController::class, 'trashed']);
-
     Route::get('/{id}', [WarehouseController::class, 'show']);
 
     Route::put('/{id}', [WarehouseController::class, 'update']);
 
     Route::post('/create', [WarehouseController::class, 'create']);
-
-    Route::post('/{id}/trash', [WarehouseController::class, 'moveToTrash']);
-
-    Route::post('/{id}/restore', [WarehouseController::class, 'restore']);
 
     Route::post('/{id}', [WarehouseController::class, 'remove']);
 });
