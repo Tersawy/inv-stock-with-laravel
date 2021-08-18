99a0798a9b01802052dcf8f1c1e86b035341ac41
tersawy <mohamedtersawy96@gmail.com>
2021-08-18 13:24:34 +0200

    Remove trashed routes

--- server/routes/api/product.php
+++ server/routes/api/product.php
@@ -17,12 +17,6 @@ Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'product'], function
 
     Route::post('/create', [ProductController::class, 'create']);
 
-    Route::post('/{id}/trash', [ProductController::class, 'moveToTrash']);
-
-    Route::get('/trashed', [ProductController::class, 'trashed']);
-
-    Route::post('/{id}/restore', [ProductController::class, 'restore']);
-
     Route::post('/{id}', [ProductController::class, 'remove']);
 });
 
